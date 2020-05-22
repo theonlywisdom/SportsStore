970154bce6ea78af24b37a8d271ecd48b62bc841
Wisdom Akwoyere Baah <50482182+theonlywisdom@users.noreply.github.com>
2020-05-22 23:18:47 +0100

    Category specific product count unit test

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -22,7 +22,8 @@ namespace SportsStore.WebUI.Controllers
 
         public ViewResult List(string category, int page = 1)
         {
-            ProductsListViewModel model = new ProductsListViewModel
+
+            ProductsListViewModel viewModel = new ProductsListViewModel
             {
                 Products = repository.Products
                     .Where(p => category == null || p.Category == category)
@@ -33,11 +34,13 @@ namespace SportsStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ?
+                        repository.Products.Count() :
+                        repository.Products.Where(e => e.Category == category).Count()
                 },
                 CurrentCategory = category
             };
-            return View(model);
+            return View(viewModel);
         }
     }
 }
